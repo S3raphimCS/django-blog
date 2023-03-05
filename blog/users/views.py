5d96087870ee39957363680c58277e14575f9e63
S3raphimCS <700520serj@mail.ru>
2023-03-06 01:07:53 +1000

    Updated templates
    Added decorated output of form errors/messages to users.
    Mainly changed users/main.css and login,signup,profile templates.

--- blog/users/views.py
+++ blog/users/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from .forms import UserLoginForm, UserRegistrationForm, UserProfileForm
-from django.contrib import auth
+from django.contrib import auth, messages
 from articles.models import Article
 from django.views.generic import DetailView
 from .models import User
@@ -8,9 +8,19 @@ from .models import User
 # TODO
 # Need to do ProfileView for an pages with unique id for example "users/profiles/2021331" instead of "profile" func
 # Because it needs access for any users to any user pages
+# Button "Admin Panel" for superusers or staff
+# Button "Create article"
+# Better decor for "Последняя статья" in profile
+# Views and rating in future
+# form.errors in signup template (HINT - position: relative for independent blocks)
+# Realize search in header
+# Model for article on moderating and add to profile template user's articles on moderating
+# Add permission for staff to moderate articles and post it on site
 
 
 def login(request):
+    if request.user.is_authenticated:
+        return redirect('profile')
     if request.method == "POST":
         form = UserLoginForm(data=request.POST)
         if form.is_valid():
@@ -28,10 +38,13 @@ def login(request):
 
 
 def sign_up(request):
+    if request.user.is_authenticated:
+        return redirect('profile')
     if request.method == 'POST':
         form = UserRegistrationForm(data=request.POST)
         if form.is_valid():
             form.save()
+            messages.success(request, 'Вы были успешно зарегистрированы!')
             return redirect('login')
     else:
         form = UserRegistrationForm()
@@ -44,8 +57,14 @@ def profile(request):
         form = UserProfileForm(instance=request.user, data=request.POST)
         if form.is_valid():
             form.save()
+            messages.success(request, 'Данные профиля успешно изменены.')
             return redirect('profile')
     else:
         form = UserProfileForm(instance=request.user)
-    context = {'form': form, 'date_joined': request.user.date_joined, 'articles': Article.objects.all().filter(author=request.user)}
+    context = {
+        'form': form,
+        'date_joined': request.user.date_joined,
+        'articles': Article.objects.all().filter(author=request.user),
+        'last_article': Article.objects.all().last()
+               }
     return render(request, 'users/profile.html', context)
